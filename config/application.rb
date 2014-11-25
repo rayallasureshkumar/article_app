11331d6e73cc41afec99aed35870f7cd9d13cf2f
rayallasureshkumar <rayakkasureshkumar@gmail.com>
2014-11-25 11:34:39 +0530

    heroku

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -52,6 +54,9 @@ module ArticleApp
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
+    #heroku
+    config.assets.debug = false
+    config.assets.initilize_on_precompile = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
